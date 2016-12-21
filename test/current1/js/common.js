f52645fe06fabbcdd8def90fa591403c385b7dfd
FlammeNoire <rikardo1618@gmail.com>
2016-12-21 16:47:21 +0200

    update

--- test/current1/js/common.js
+++ test/current1/js/common.js
@@ -1,20 +1,32 @@
 /**
  * Created by Жека on 27.07.2016.
  */
-$(function() {
+(function($) {
     $('.section-first-content .content-item').equalHeights();
     $('.faq-item').equalHeights();
-    $('.timer').startTimer({
-        onComplete: function(element){
-            element.addClass('is-complete');
-        },
-        loop: true,
-        loopInterval: 1,
-    });
+
+    var isIE = document.body.style.msTouchAction !== undefined;
+
+    if(!isIE) {
+
+        $('.timer').startTimer({
+            onComplete: function(element){
+                element.addClass('is-complete');
+            },
+            loop: true,
+            loopInterval: 1
+        });
+
+    } else {
+        $('.timer').html('00:59:55');
+    }
+
+
     $(".order-button").mPageScroll2id({
         scrollSpeed: 700,
         scrollEasing: "linear"
     });
+
 
     $( '#my-slider' ).sliderPro({
         width: 600,
@@ -27,4 +39,4 @@ $(function() {
         thumbnailHeight: 66,
         thumbnailsPosition: 'left'
     });
-});
+})(jQuery);
