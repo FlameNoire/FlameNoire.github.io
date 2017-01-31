48eed32aad8aeb56a60ee31e5c35e59ce7e8bbc3
FlammeNoire <rikardo1618@gmail.com>
2017-01-31 20:41:03 +0200

    0

---
+++ test/current55/js/common.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Жека on 27.07.2016.
+ */
+(function($) {
+
+    $('.reviews_slider').slick({
+        dots: false,
+        infinite: true,
+        // slidesToShow: 3,
+        // slidesToScroll: 3,
+        // centerMode: true,
+        variableWidth: true,
+        adaptiveHeight: true,
+        speed: 600
+    });
+
+})(jQuery);
