6f61f2766c3c02b1777c311a8f994c747d82c05f
flammenoire <rikardo1618@gmail.com>
2019-07-27 03:59:37 +0300

    add new one

--- test/sportextrim/js/common.js
+++ test/sportextrim/js/common.js
@@ -1,17 +1,38 @@
 (function($) {
 
     var $slider = $('.s_top_slider');
-
     $slider.slick({
         dots: true,
         arrows: true,
         infinite: true
     });
 
-    $('.enter_btn').click(function (e) {
-        e.preventDefault();
-        $('.authorization_form').fadeIn(500);
+    var $slider2 = $('.shops_slider');
+    $slider2.slick({
+        dots: false,
+        arrows: true,
+        prevArrow: ".shops_arrow_left",
+        nextArrow: ".shops_arrow_right",
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
     });
+
+
+    var button = $('.catalog_menu_btn');
+    var menu = $('.catalog_menu');
+    var menuLink = $('.catalog_menu ul>li>a');
+    $('.catalog_menu_btn').click(function () {
+        button.toggleClass("active");
+        menu.toggleClass("active");
+    });
+    menuLink.click(function () {
+        button.toggleClass("active");
+        menu.toggleClass("active");
+    })
+
     $('.authorization_form .close_btn').click(function (e) {
         e.preventDefault();
         $('.authorization_form').fadeOut(300);
